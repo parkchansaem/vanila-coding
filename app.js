09c8ce7b6981de19d6a31e8453c89f114241e831
chansaem <ased1233@naver.com>
2022-02-07 22:40:27 +0900

    start

---
+++ app.js
@@ -0,0 +1,21 @@
+const calaulration = {
+  add: function add(a, b) {
+    console.log(a + b);
+  },
+  minus: function minus(a, b) {
+    console.log(a - b);
+  },
+  mul: function mul(a, b) {
+    console.log(a * b);
+  },
+  div: function div(a, b) {
+    console.log(a / b);
+  },
+};
+
+const a = 10;
+const b = 5;
+calaulration.add(a, b);
+calaulration.minus(a, b);
+calaulration.mul(a, b);
+calaulration.div(a, b);
