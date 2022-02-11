b01215ed459fe9c7b45dc89b5d720b18cb08e54a
chansaem <ased1233@naver.com>
2022-02-12 01:13:24 +0900

    banillajs_code_challenge#2

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
-const h1 = document.querySelector("#title h1");
-console.dir(h1);
-
 const superEventHandler = {
-  click: function clickEvent() {
-    h1.style.color = "blue";
-  },
-  enter: function mouseEnter() {
-    h1.innerText = "the mouse is here!";
-    h1.style.color = "red";
-  },
-  enter: function mouseEnter() {
-    h1.innerText = "the mouse is here!";
-    h1.style.color = "red";
-  },
-  leave: function mouseLeave() {
-    h1.innerText = "the mouse is gone";
-    h1.style.color = "skyblue";
-  },
-  resize: function handleResize() {
-    h1.innerText = "you just resized";
-    h1.style.color = "yellow";
-  },
-  rightclick: function windowContext() {
-    h1.innerText = "that was a right click";
-    h1.style.color = "green";
+  resize: function () {
+    if (window.innerWidth < 600) {
+      document.body.style.background = "red";
+    } else if (window.innerWidth < 800) {
+      document.body.style.background = "yellow";
+    } else if (window.innerWidth < 1000) {
+      document.body.style.background = "green";
+    } else {
+      document.body.style.background = "blue";
+    }
   },
 };
 
-h1.addEventListener("click", superEventHandler.click);
-h1.addEventListener("mouseenter", superEventHandler.enter);
-h1.addEventListener("mouseleave", superEventHandler.leave);
 window.addEventListener("resize", superEventHandler.resize);
-window.addEventListener("contextmenu", superEventHandler.rightclick);
