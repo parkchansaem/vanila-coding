ba9c8356927e721b047c2ad9922e92a7863c5269
chansaem <ased1233@naver.com>
2022-02-12 23:31:52 +0900

    javascript_study#3

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const superEventHandler = {
-  resize: function () {
-    if (window.innerWidth < 600) {
-      document.body.style.background = "red";
-    } else if (window.innerWidth < 800) {
-      document.body.style.background = "yellow";
-    } else if (window.innerWidth < 1000) {
-      document.body.style.background = "green";
-    } else {
-      document.body.style.background = "blue";
-    }
-  },
-};
+const loginform = document.querySelector("#login");
+const logininput = document.querySelector("#login input");
+const textuser = document.querySelector("#textuser");
 
-window.addEventListener("resize", superEventHandler.resize);
+function handlesubmit(info) {
+  info.preventDefault();
+  console.log(info);
+  loginform.classList.add("hidden");
+  const user = logininput.value;
+  textuser.innerText = `hello${user}`;
+  textuser.classList.remove("hidden");
+}
+
+loginform.addEventListener("submit", handlesubmit);
