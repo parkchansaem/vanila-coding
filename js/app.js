014113538d7fd8d71553b07c225170bf1453ac12
chansaem <ased1233@naver.com>
2022-02-13 15:20:30 +0900

    javascript_study#4

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const loginform = document.querySelector("#login");
+const logininput = document.querySelector("#login input");
+const textuser = document.querySelector("#textuser");
+
+function handlesubmit(info) {
+  info.preventDefault();
+  loginform.classList.add("hidden");
+  const user = logininput.value;
+  localStorage.setItem("username", user);
+  userdisplay();
+}
+function userdisplay() {
+  const userinfo = localStorage.getItem("username");
+  textuser.innerText = `hello${userinfo}`;
+  textuser.classList.remove("hidden");
+}
+const saveuserinfo = localStorage.getItem("username");
+
+if (saveuserinfo === null) {
+  loginform.classList.remove("hidden");
+  loginform.addEventListener("submit", handlesubmit);
+} else {
+  userdisplay();
+}
---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function timeclock() {
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hour}:${minute}:${second}`;
+  console.log(date.getSeconds());
+}
+timeclock();
+setInterval(timeclock, 1000);
