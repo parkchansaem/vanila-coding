765a646e9d294e466c05de50c0aacd06a66abd58
chansaem <ased1233@naver.com>
2022-02-28 14:28:44 +0900

    calendar

---
+++ js/calendar.js
@@ -0,0 +1,29 @@
+const calendar = document.querySelector("#calendar");
+const month = document.querySelector("div#month");
+const day = document.querySelector("div#day");
+const date = document.querySelector("div#date");
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const WEEKDAY = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+function calenderNow() {
+  const now = new Date();
+  const dataMonth = monthNames[now.getMonth()].slice(0, 3);
+  month.innerText = `${dataMonth}`;
+  const dateDay = WEEKDAY[now.getDay()];
+  day.innerText = `${dateDay}`;
+  console.log(day);
+}
+calenderNow();
+setInterval(calenderNow, 1000);
--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,8 @@ function timeclock() {
   const date = new Date();
   const hour = String(date.getHours()).padStart(2, "0");
   const minute = String(date.getMinutes()).padStart(2, "0");
-  const second = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hour}:${minute}:${second}`;
+
+  clock.innerText = `${hour}:${minute}`;
   console.log(date.getSeconds());
 }
 timeclock();
